81d661d37799d43b1da20017138e9993ac8421c8
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2026-03-16 23:55:11 +0400

    adding data and orm

---
+++ drizzle.config.ts
@@ -0,0 +1,16 @@
+import { config } from "dotenv";
+import { defineConfig } from "drizzle-kit";
+config({ path: ".env.local" });
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set in the .env file");
+}
+
+export default defineConfig({
+  schema: "./src/schema.ts", // Your schema file path
+  out: "./src/db/drizzle", // Your migrations folder
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
+});
